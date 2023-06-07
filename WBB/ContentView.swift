fbdf85f5de8bf68aed6f0522d2df6f6ff10f242c
kimpepe <unboxing96@gmail.com>
2023-06-08 08:53:19 +0900

    initial commit

---
+++ WBB/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WBB
+//
+//  Created by kimpepe on 2023/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WBB/WBBApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBBApp.swift
+//  WBB
+//
+//  Created by kimpepe on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct WBBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
